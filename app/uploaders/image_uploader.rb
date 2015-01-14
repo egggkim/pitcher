71d821bfa7d82922975a94acaddcc34531d114fe
Karen S Kim <karenskim19@gmail.com>
2015-01-13 19:15:22 -0800

    add style to pitches#index view

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -36,7 +36,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
   version :large do
-    process :resize_to_fit => [650, nil]
+    process :resize_to_fit => [500, nil]
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
