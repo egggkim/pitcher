14810fad3d71882c0597c75c504be59f54ec6a40
Karen S Kim <karenskim19@gmail.com>
2015-01-13 19:55:51 -0800

    update user avatar img and filter form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,14 @@
 class CommentsController < ApplicationController
   def create
     pitch = Pitch.find(params[:pitch_id])
-    comment = pitch.comments.create(comment_params)
+    pitch_comment = comment_params
+    pitch_comment[:commenter] = current_user.first_name
+    comment = pitch.comments.create(pitch_comment)
     redirect_to pitch_path(comment.pitch)
   end
 
   private
   def comment_params
-    params.require(:comment).permit(:body, commenter_attributes: [:name, :email])
+    params.require(:comment).permit(:body)
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,12 +2,10 @@ class Comment
   include Mongoid::Document
   include Mongoid::Timestamps 
   field :body, type: String
+  field :commenter, type: String
 
   embedded_in :pitch
-  embeds_one :commenter
 
-  accepts_nested_attributes_for :commenter
-  
   def date_added 
     created_at.localtime.strftime("%-m/%-d/%Y | %l:%M %p") 
   end
