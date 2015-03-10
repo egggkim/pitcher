c6cc9ee4b0280e170cdb3bc9f161615ddf9a94c9
Karen S Kim <karenskim19@gmail.com>
2015-03-10 14:21:48 -0700

    remove contact page and add direct email link to footer

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,6 @@ Rails.application.routes.draw do
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
   get '/about' => 'welcome#about'
-  get '/contact' => 'welcome#contact'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
