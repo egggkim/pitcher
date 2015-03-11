65dde748dd115fa52b0cc14da71538d2a8ffbd28
Karen S Kim <karenskim19@gmail.com>
2015-03-10 17:28:15 -0700

    update pitch edit error view

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   end
 
   root 'welcome#index'
+  get '/', to: redirect('/pitches')
   get '/signup' => 'users#new'
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
